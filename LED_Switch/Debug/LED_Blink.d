57496a0b4a5142b94a8147c900bfd6367e36bbeb
Avinash Damse <avinashdamse11@gmail.com>
2022-11-01 11:29:43 -0700

    New1

---
+++ LED_Switch/Debug/LED_Blink.d
@@ -0,0 +1,60 @@
+# FIXED
+
+LED_Blink.obj: ../LED_Blink.c
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/msp.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/msp432p401r.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/msp_compatibility.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/msp432p401r_classic.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+LED_Blink.obj: C:/ti/ccs1210/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+../LED_Blink.c:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1210/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
